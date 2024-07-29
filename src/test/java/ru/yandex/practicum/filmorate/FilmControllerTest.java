0c501a42776bc3eccb8db7238668b11ce3ef234e
Mihalina Gotovchits <Mix-Mihalina@yandex.ru>
2024-07-29 13:19:32 +0300

    Выполнила реализацию тз №10 в соответсвии с условиями

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -7,7 +7,6 @@ import ru.yandex.practicum.filmorate.exception.LineLengthException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.util.Collection;
 
